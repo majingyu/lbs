69942e99ce0d1942040c5772722f0df57fc1ac29
wxm970826 <wangximing@heaptechnology.com>
2013-12-03 12:03:50 +0800

    add select title, add setLeftTitle、setRightTitle functions

--- lbs/js/listBoxSelector.js
+++ lbs/js/listBoxSelector.js
@@ -14,6 +14,9 @@
                 	var defaultOptions = {
                 		left: [],
                 		right: [],
+                		leftTitle: '',
+                		rightTitle: '',
+                		sort: false,
                 		confirm: null
                 	};
                 	
@@ -97,10 +100,10 @@
         	}
         	
         	var self = this;
-        	var options = $(this).data('options');
+        	var options = $(self).data('options');
         	options.left = items;
-        	$(this).data('options', options);
-        	var leftSelect = $(this).find('.lbs-left-select');
+        	$(self).data('options', options);
+        	var leftSelect = $(self).find('.lbs-left-select');
         	var option = null;
         	
         	// empty left select
@@ -123,6 +126,25 @@
         },
         
         /**
+         * set left select title
+         * 
+         * @author Ximing Wang
+         */
+        setLeftTitle: function (title) {
+        	if (title == undefined || title == null) {
+        		return;
+        	}
+        	
+        	var self = this;
+        	var options = $(self).data('options');
+        	options.leftTitle = title;
+        	$(self).data('options', options);
+        	
+        	var leftTitleElement = $(self).find('.lbs-left-title');
+        	leftTitleElement.html(title);
+        },
+        
+        /**
          * set right select items
          * 
          * @author Ximing Wang
@@ -133,10 +155,10 @@
         	}
         	
         	var self = this;
-        	var options = $(this).data('options');
+        	var options = $(self).data('options');
         	options.right = items;
-        	$(this).data('options', options);
-        	var rightSelect = $(this).find('.lbs-right-select');
+        	$(self).data('options', options);
+        	var rightSelect = $(self).find('.lbs-right-select');
         	var option = null;
         	
         	// empty right empty
@@ -156,6 +178,25 @@
         	});
         	
         	changeBtnStatus(self);
+        },
+        
+        /**
+         * set right select title
+         * 
+         * @author Ximing Wang
+         */
+        setRightTitle: function (title) {
+        	if (title == undefined || title == null) {
+        		return;
+        	}
+        	
+        	var self = this;
+        	var options = $(self).data('options');
+        	options.rightTitle = title;
+        	$(self).data('options', options);
+        	
+        	var rightTitleElement = $(self).find('.lbs-right-title');
+        	rightTitleElement.html(title);
         }
     };
     
@@ -166,7 +207,10 @@
      */
     var render = function (instance) {
     	var html = '<div class="lbs-content">' +
-	    		'<div class="lbs-left-container"><select multiple class="form-control lbs-left-select"></select></div>' +
+	    		'<div class="lbs-left-container">' +
+	    			'<div class="lbs-left-title"></div>' +
+	    			'<select multiple class="form-control lbs-left-select"></select>' +
+    			'</div>' +
 	    		'<div class="lbs-center-container">' +
 	    			'<div class="lbs-change-btns-split"></div>' +
 	    			'<div class="lbs-change-btns">' +
@@ -184,7 +228,10 @@
 	    				'</div>' +
 	    			'</div>' +
 	    		'</div>' +
-	    		'<div class="lbs-right-container"><select multiple class="form-control lbs-right-select"></select></div>' +
+	    		'<div class="lbs-right-container">' +
+	    			'<div class="lbs-right-title"></div>' +
+	    			'<select multiple class="form-control lbs-right-select"></select>' +
+    			'</div>' +
 	    	'</div>';
     	var options = $(instance).data('options');
     	var confirm = options.confirm;
@@ -208,8 +255,12 @@
     	var options = $(instance).data('options');
     	var leftItems = options.left;
     	var leftSelect = $(instance).find('.lbs-left-select');
+    	var leftTitle = options.leftTitle;
+    	var leftTitleElement = $(instance).find('.lbs-left-title');
     	var rightItems = options.right;
     	var rightSelect = $(instance).find('.lbs-right-select');
+    	var rightTitle = options.rightTitle;
+    	var rightTitleElement = $(instance).find('.lbs-right-title');
     	
     	var option = null;
     	
@@ -225,6 +276,9 @@
     		}
     		leftSelect.append(option);
     	});
+
+    	// init left title
+    	leftTitleElement.html(leftTitle);
     	
     	// init right select data
     	$.each(rightItems, function (index, item) {
@@ -238,6 +292,9 @@
     		}
     		rightSelect.append(option);
     	});
+    	
+    	// init right title
+    	rightTitleElement.html(rightTitle);
     };
 
     /**
@@ -246,83 +303,128 @@
     var initializeEvents = function (instance) {
     	var leftSelect = $(instance).find('.lbs-left-select');
     	var rightSelect = $(instance).find('.lbs-right-select');
+    	var leftOptions = leftSelect.find('option');
+    	var rightOptions = rightSelect.find('option');
     	var addOne = $(instance).find('.lbs-add-one');
     	var addAll = $(instance).find('.lbs-add-all');
     	var removeOne = $(instance).find('.lbs-remove-one');
     	var removeAll = $(instance).find('.lbs-remove-all');
+    	var options = $(instance).data('options');
+    	
+    	// left select change event
+    	leftSelect.unbind('change');
+    	leftSelect.bind('change', function () {
+    		addOne.removeClass('disabled');
+    		cancelSelected(instance, rightSelect);
+    		changeBtnStatus(instance);
+    	});
+    	
+    	// right select change event
+    	rightSelect.unbind('change');
+    	rightSelect.bind('change', function () {
+    		removeOne.removeClass('disabled');
+    		cancelSelected(instance, leftSelect);
+    		changeBtnStatus(instance);
+    	});
     	
     	// addOne button click event
-    	addOne.on('click', function () {
+    	addOne.unbind('click');
+    	addOne.bind('click', function (event) {
+    		if ($(this).hasClass('disabled')) {
+    			// if button is disabled, return
+    			event.preventDefault();
+    			return false;
+    		}
+    		
     		// find the selected option
     		var selected = leftSelect.find('option:selected');
-    		if (selected.length == 0 && leftSelect.find('option').length > 0) {
-    			// if have no selected option and left select has options,
-    			// selected the first one
-    			selected = leftSelect.find('option:nth-child(1)');
-    		}
     		
     		if (selected.length > 0) {
     			// if has the selected option,move it to selected select
     			rightSelect.append(selected);
     			
     			// sort
-    			selectSort(instance, rightSelect);
+    			if (options.sort) {
+    				selectSort(instance, rightSelect);
+    			}
     		}
     		
-    		changeBtnStatus(instance);
+    		cancelSelected(instance, instance);
     		
-    		cancelAllSelected(instance);
+    		changeBtnStatus(instance);
     	});
     	
     	// addAll button click event
-    	addAll.on('click', function () {
+    	addAll.unbind('click');
+    	addAll.bind('click', function (event) {
+    		if ($(this).hasClass('disabled')) {
+    			// if button is disabled, return
+    			event.preventDefault();
+    			return false;
+    		}
+    		
     		// move all the options to selected select
     		var options = leftSelect.find('option');
     		rightSelect.append(options);
     		
     		// sort
-			selectSort(instance, rightSelect);
-			
-			changeBtnStatus(instance);
-			
-			cancelAllSelected(instance);
+    		if (options.sort) {
+    			selectSort(instance, rightSelect);
+    		}
+    		
+    		cancelSelected(instance, instance);
+    		
+    		changeBtnStatus(instance);
     	});
     	
     	// removeOne button click event
-    	removeOne.on('click', function () {
+    	removeOne.unbind('click');
+    	removeOne.bind('click', function (event) {
+    		if ($(this).hasClass('disabled')) {
+    			// if button is disabled, return
+    			event.preventDefault();
+    			return false;
+    		}
+    		
     		// find the selected option
     		var selected = rightSelect.find('option:selected');
-    		if (selected.length == 0 && rightSelect.find('option').length > 0) {
-    			// if have no selected option and selected select has options,
-    			// selected the first one
-    			selected = rightSelect.find('option:nth-child(1)');
-    		}
     		
     		if (selected.length > 0) {
     			// if has the selected option,move it to left select
     			leftSelect.append(selected);
     			
     			// sort
-    			selectSort(instance, leftSelect);
+    			if (options.sort) {
+    				selectSort(instance, leftSelect);
+    			}
     		}
+
+    		cancelSelected(instance, instance);
     		
     		changeBtnStatus(instance);
-    		
-    		cancelAllSelected(instance);
     	});
     	
     	// removeAll button click event
-    	removeAll.on('click', function () {
+    	removeAll.unbind('click');
+    	removeAll.bind('click', function (event) {
+    		if ($(this).hasClass('disabled')) {
+    			// if button is disabled, return
+    			event.preventDefault();
+    			return false;
+    		}
+    		
     		// move all the options to left select
     		var options = rightSelect.find('option');
     		leftSelect.append(options);
     		
     		// sort
-			selectSort(instance, leftSelect);
-			
-			changeBtnStatus(instance);
-			
-			cancelAllSelected(instance);
+    		if (options.sort) {
+    			selectSort(instance, leftSelect);
+    		}
+
+    		cancelSelected(instance, instance);
+    		
+    		changeBtnStatus(instance);
     	});
     	
     	changeBtnStatus(instance);
@@ -332,7 +434,8 @@
     	if (confirm != null && confirm.callback != undefined && confirm.callback != null) {
     		// if has confirm button,callback
     		var confirmBtn = $(instance).find('.lbs-confirm-btn');
-    		confirmBtn.on('click', function () {
+    		confirmBtn.unbind('click');
+    		confirmBtn.bind('click', function () {
     			var leftOptions = leftSelect.find('option');
     			var rightOptions = rightSelect.find('option');
     			var leftItems = [];
@@ -353,12 +456,12 @@
     };
     
     /**
-     * cansel all options selected
+     * cancel select options selected
      * 
      * @author Ximing Wang
      */
-    var cancelAllSelected = function (instance) {
-    	var options = $(instance).find('option');
+    var cancelSelected = function (isntance, select) {
+    	var options = $(select).find('option');
     	$.each(options, function (index, option) {
     		option.selected = false;
     	});
@@ -370,30 +473,46 @@
      * @author Ximing Wang
      */
     var changeBtnStatus = function (instance) {
-    	var leftOptions = $(instance).find('.lbs-left-select').find('option');
-    	var rightOptions = $(instance).find('.lbs-right-select').find('option');
+    	var leftSelect = $(instance).find('.lbs-left-select');
+    	var rightSelect = $(instance).find('.lbs-right-select');
+    	var leftOptions = leftSelect.find('option');
+    	var rightOptions = rightSelect.find('option');
+    	var leftSelected = leftSelect.find('option:selected');
+    	var rightSelected = rightSelect.find('option:selected');
     	var addOne = $(instance).find('.lbs-add-one');
     	var addAll = $(instance).find('.lbs-add-all');
     	var removeOne = $(instance).find('.lbs-remove-one');
     	var removeAll = $(instance).find('.lbs-remove-all');
     	
-    	// disable or enable addOne button and addAll button
-    	if (leftOptions.length > 0) {
+    	// disable or enable addOne button
+    	if (leftSelected.length > 0) {
     		addOne.removeClass('disabled');
-    		addAll.removeClass('disabled');
     	}
     	else {
     		addOne.addClass('disabled');
+    	}
+    	
+    	// disable or enable addAll button
+    	if (leftOptions.length > 0) {
+    		addAll.removeClass('disabled');
+    	}
+    	else {
     		addAll.addClass('disabled');
     	}
     	
-    	// disable or enable removeOne button and removeAll button
-    	if (rightOptions.length > 0) {
+    	// disable or enable removeOne button
+    	if (rightSelected.length > 0) {
     		removeOne.removeClass('disabled');
-    		removeAll.removeClass('disabled');
     	}
     	else {
     		removeOne.addClass('disabled');
+    	}
+    	
+    	// disable or enable removeAll button
+    	if (rightOptions.length > 0) {
+    		removeAll.removeClass('disabled');
+    	}
+    	else {
     		removeAll.addClass('disabled');
     	}
     };
@@ -455,6 +574,16 @@
     		changeBtnElement.css('margin-top', container.height() * 0.15 + 'px');
     	});
     	
+    	// init titles css
+    	var leftTitle = $(instance).find('.lbs-left-title');
+    	var height = leftTitle.height();
+    	leftTitle.css('font-size', height * 0.8 + 'px');
+    	leftTitle.css('line-height', height * 0.9 + 'px');
+    	var rightTitle = $(instance).find('.lbs-right-title');
+    	height = rightTitle.height();
+    	rightTitle.css('font-size', height * 0.8 + 'px');
+    	rightTitle.css('line-height', height * 0.9 + 'px');
+    	
     	$(window).resize(function () {
     		var changeBtns = $(instance).find('.lbs-change-btn');
         	var container = $(changeBtns[0]).parent();
@@ -469,6 +598,16 @@
         		changeBtnElement.css('line-height', containerHeight + 'px');
         		changeBtnElement.css('margin-top', container.height() * 0.15 + 'px');
         	});
+        	
+        	// init titles css
+        	var leftTitle = $(instance).find('.lbs-left-title');
+        	var height = leftTitle.height();
+        	leftTitle.css('font-size', height * 0.8 + 'px');
+        	leftTitle.css('line-height', height * 0.9 + 'px');
+        	var rightTitle = $(instance).find('.lbs-right-title');
+        	height = rightTitle.height();
+        	rightTitle.css('font-size', height * 0.8 + 'px');
+        	rightTitle.css('line-height', height * 0.9 + 'px');
     	});
     };
     
@@ -488,6 +627,18 @@
     	
     	if (options.right != undefined && options.right != null) {
     		defaultOptions.right = options.right;
+    	}
+    	
+    	if (options.leftTitle != undefined && options.leftTitle != null) {
+    		defaultOptions.leftTitle = options.leftTitle;
+    	}
+    	
+    	if (options.rightTitle != undefined && options.rightTitle != null) {
+    		defaultOptions.rightTitle = options.rightTitle;
+    	}
+    	
+    	if (options.sort != undefined && options.sort != null) {
+    		defaultOptions.sort = options.sort;
     	}
     	
     	if (options.confirm != undefined && options.confirm != null) {
